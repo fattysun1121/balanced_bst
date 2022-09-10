3c7dc55168509553060fcb7cab114608d42d7a18
fattysun1121 <85023400+fattysun1121@users.noreply.github.com>
2022-09-10 17:09:24 -0400

    Finish #delete and two other helper methods for recursive functionality

--- balanced_bst.rb
+++ balanced_bst.rb
@@ -1,19 +1,19 @@
-class Node
-include Comparable
-attr_accessor :data, :left, :right
+# frozen_string_literal: true
 
-  def initialize(data, left=nil, right=nil)
+class Node
+  include Comparable
+  attr_accessor :data, :left, :right
+
+  def initialize(data, left = nil, right = nil)
     @data = data
     @left = left
     @right = right
   end
 
-  def <=>(other_node)
-    data <=> other_node.data
-    
+  def <=>(other)
+    data <=> other.data
   end
 end
-
 
 class Tree
   def initialize(array)
@@ -33,25 +33,28 @@ class Tree
     end
   end
 
-  def insert(node)
-    unless @array.include?(node.data)
+  def insert(value)
+    node = Node.new value
+    unless @array.include?(value)
       current_node = @root
       while current_node.left != node && current_node.right != node
         if node > current_node
-          unless current_node.right.nil?
-            current_node = current_node.right
-          else
+          if current_node.right.nil?
             current_node.right = node
-          end
-        else
-          unless current_node.left.nil?
-            current_node = current_node.left
           else
-            current_node.left = node
+            current_node = current_node.right
           end
+        elsif current_node.left.nil?
+          current_node.left = node
+        else
+          current_node = current_node.left
         end
-      end  
+      end
     end
+  end
+
+  def delete(val)
+    @root = delete_rec(@root, val)
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
@@ -59,10 +62,39 @@ class Tree
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
+
+  private
+
+  def delete_rec(root, val)
+    if root.nil?
+      return root
+    end
+
+    if val < root.data
+      root.left = delete_rec(root.left, val)
+    elsif val > root.data
+      root.right = delete_rec(root.right, val)
+    elsif root.left.nil?
+      return root.right
+    elsif root.right.nil?
+      return root.left
+    else
+      root.data = min_value(root.right)
+      root.right = delete_rec(root.right, root.data)
+    end
+    root
+  end
+
+  def min_value(root)
+    minv = root.data
+    until root.left.nil?
+      minv = root.left.data
+      root = root.left
+    end
+    minv
+  end
 end
 
 t = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+
 t.pretty_print
-
-
-
